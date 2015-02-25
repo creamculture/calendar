800f3e6542da5924b5693efb3d7fba171147221d
Kaiyi <khuang@email.wm.edu>
2015-02-24 20:16:37 -0500

    profile picture selection supposed to be working now

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@
     database.getDb = function(next){
         if(!theDb) {
             //connect to the database
-            mongodb.MongoClient.connect('mongodb://localhost/nodetest1', function (err, db) {
+            mongodb.MongoClient.connect('mongodb://admin:admin@ds053838.mongolab.com:53838/heroku_app33580717', function (err, db) {
                 if(err){
                     next(null);
                 } else {
